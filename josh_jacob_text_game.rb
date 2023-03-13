b7f9ed01d48af86e338f1d1fbd24d4bb593ac995
JoshuaMaurer <maurer.joshua@gmail.com>
2023-03-13 14:17:08 -0700

    added updated planning screen shot to this branch.

--- josh_jacob_text_game.rb
+++ josh_jacob_text_game.rb
@@ -1,6 +1,5 @@
 p 'starting screen Crash site in front of a lake are you up for a challange ? yes or no '
 userinput = gets.chomp
-
 def first_choice (userinput)
     if userinput.downcase == 'yes'
         'fantastic lets continue to the oasis'
@@ -8,17 +7,13 @@ def first_choice (userinput)
        p "you're gonna starve to death"
         exit 1
     end
-
-
 end
 p first_choice(userinput)
-p 'As the player you see 3 paths  straight ,right and left. Choose wisely 👽 '
 
+p 'As the player you see 3 paths  straight ,right and left. Choose wisely 👽 '
 p 'left arrund the lake  , straight swim acrioss the lake, right around the lake '
 p 'What is your choice'
 userinput = gets.chomp
-
-
 def sec_choice (userinput)
     if userinput.downcase == "right"
         'fantastic lets continue to the oasis'
@@ -31,16 +26,13 @@ def sec_choice (userinput)
     else  
       "you choose not to leave this location so you starved to death"
     end
-
 end
-
 p sec_choice(userinput)
 
 p 'As the player you see 3 paths  straight ,right and left. Choose wisely 👽 '
 p 'left leads to the Lake  , straight leads down a dangerous path, right leads towards the mountian '
 p 'What is your choice'
 userinput = gets.chomp
-
 def third_choice (userinput)
     if userinput.downcase == "right"
         'fantastic lets continue to the oasis'
@@ -53,18 +45,13 @@ def third_choice (userinput)
     else  
      "you choose not to leave this location so you starved to death"
     end
-
 end
 p third_choice(userinput)
-
-
-
 
 p 'As the player you see 3 paths  straight ,right and left. Choose wisely 👽 '
 p 'left leads to the village  , straight leads to the mountain path, right leads to a river '
 p 'What is your choice'
 userinput = gets.chomp
-
 def forth_choice (userinput)
     if userinput.downcase == "straight"
         'fantastic lets continue to the oasis'
@@ -77,23 +64,13 @@ def forth_choice (userinput)
     else  
      "you choose not to leave this location so you starved to death"
     end
-
 end
 p forth_choice(userinput)
-
-
-
-
-
-
-
-
 
 p 'As the player you see 3 paths  straight ,right and left. Choose wisely 👽 '
 p 'left leads to the snow covered mountians  , straight leads to a bunch of loose and scrry rock, right leads to a cave entrance '
 p 'What is your choice'
 userinput = gets.chomp
-
 def fifth_choice (userinput)
     if userinput.downcase == "right"
         'Fantastic lets continue to the oasis'
@@ -106,19 +83,13 @@ def fifth_choice (userinput)
     else  
      "you choose not to leave this location so you starved to death"
     end
-
 end
 p fifth_choice(userinput)
-
-
-
-
 
 p 'As the player you see 3 paths  straight ,right and left. Choose wisely 👽 '
 p 'left you see a light at the end of the Cave  , straight rivien , right is a sarm of small insects'
 p 'What is your choice'
 userinput = gets.chomp
-
 def sixth_choice (userinput)
     if userinput.downcase == "left"
         'you win'
@@ -131,6 +102,5 @@ def sixth_choice (userinput)
     else  
      "you choose not to leave this location so you starved to death"
     end
-
 end
 p sixth_choice(userinput)
